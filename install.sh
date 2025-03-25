6758079c4ce10204e4f6870fa6b3ac7440ae4f3d
Marcus Martins <marcuspaivamartins@gmail.com>
2025-03-25 01:31:24 -0300

    fix: Correção de sintaxe bash e biblioteca DBus - Corrige if na verificação do sudo - Correção das instruções de uso - Correção do nome da biblioteca DBus - Adiciona -y para aceitar instalação de pacotes

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Verifica se está executando com sudo, se não pede para que faça
-if [[ "$EUID" -ne 0 ]]
+if [[ "$EUID" -ne 0 ]]; then
 	echo "O script precisa de permissões de root!"
-	echo "Uso: sudo $0"
+	echo "Uso: sudo ./install.sh"
 	exit 1
 fi
 
@@ -11,19 +11,19 @@ fi
 distro=$(grep "^ID=" /etc/os-release | awk -F= '{print tolower($2)}' | tr -d '"')
 
 # Define os pacotes com base nas distros-mãe
-pacotes_debian_base="pulseaudio-utils dbus-python python3-dbus google-chrome-stable"
-pacotes_rhel_base="pulseaudio-utils dbus-python python3-dbus google-chrome-stable"
+pacotes_debian_base="pulseaudio-utils dbus-python-devel python3-dbus google-chrome-stable"
+pacotes_rhel_base="pulseaudio-utils dbus-python-devel python3-dbus google-chrome-stable"
 
 # TODO: testei os IDs só para rhel, fedora, debian, ubuntu e vi que no centos às vezes tem IDs diferentes a depender da versão, mas deixei a "padrão"
 
 # Instala os pacotes de acordo com a distribuição
 if [[ "$distro" == "ubuntu" || "$distro" == "debian" ]]; then
 	apt update -y
-	apt install $pacotes_debian_base
+	apt install -y $pacotes_debian_base
 
 elif [[ "$distro" == "rhel" || "$distro" == "fedora" || "$distro" == "centos" ]]; then
 	dnf update -y
-	dnf install $pacotes_rhel_base
+	dnf install -y $pacotes_rhel_base
 
 else
 	echo "Distribuição não suportada, adicione sua distribuição \"$distro\"  ao código"
