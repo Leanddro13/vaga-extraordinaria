5431e46d68cc2384ff99bb84770883c26253a160
Marcus Martins <marcuspaivamartins@gmail.com>
2025-03-24 22:34:04 -0300

    fix: Permite rodar em distros baseadas em RHEL TODO: Verificar todas possibilidades de IDs do CentOS no /etc/os-release

--- install.sh
+++ install.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install pulseaudio-utils
+# Verifica se está executando com sudo, se não pede para que faça
+if [[ "$EUID" -ne 0 ]]
+	echo "O script precisa de permissões de root!"
+	echo "Uso: sudo $0"
+	exit 1
+fi
+
+# Pega o nome da distro pelo os-release
+distro=$(grep "^ID=" /etc/os-release | awk -F= '{print tolower($2)}' | tr -d '"')
+
+# Define os pacotes com base nas distros-mãe
+pacotes_debian_base="pulseaudio-utils dbus-python python3-dbus google-chrome-stable"
+pacotes_rhel_base="pulseaudio-utils dbus-python python3-dbus google-chrome-stable"
+
+# TODO: testei os IDs só para rhel, fedora, debian, ubuntu e vi que no centos às vezes tem IDs diferentes a depender da versão, mas deixei a "padrão"
+
+# Instala os pacotes de acordo com a distribuição
+if [[ "$distro" == "ubuntu" || "$distro" == "debian" ]]; then
+	apt update -y
+	apt install $pacotes_debian_base
+
+elif [[ "$distro" == "rhel" || "$distro" == "fedora" || "$distro" == "centos" ]]; then
+	dnf update -y
+	dnf install $pacotes_rhel_base
+
+else
+	echo "Distribuição não suportada, adicione sua distribuição \"$distro\"  ao código"
+	exit 1
+fi
 
 pip install -r requirements.txt
+
